a272e316af315dd63dc3ffc286aaeefcfed40586
Lucky Jain <72547660+lostb053@users.noreply.github.com>
2024-07-02 22:57:15 +0200

    Update mirror_leech.py
    Signed-off-by: Lucky Jain <72547660+lostb053@users.noreply.github.com>

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -204,7 +204,7 @@ async def _mirror_leech(client, message, isQbit=False, isLeech=False, sameDir=No
         file_ = getattr(reply_to, reply_to.media.value) if reply_to.media else None
         if file_ is None and reply_to.text:
             reply_text = reply_to.text.split('\n', 1)[0].strip()
-            if is_url(reply_text) or is_magnet(reply_text):
+            if is_url(reply_text) or is_magnet(reply_text, True):
                 link = reply_text
         elif reply_to.document and (file_.mime_type == 'application/x-bittorrent' or file_.file_name.endswith('.torrent')):
             link = await reply_to.download()
